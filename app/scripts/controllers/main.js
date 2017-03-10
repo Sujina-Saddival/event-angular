413863eb4528fab634a0dc4217230fa77959256b
Sujina <shalesh@qwinix.io>
2017-03-10 14:59:08 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eventAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eventAngularApp
+ */
+angular.module('eventAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
